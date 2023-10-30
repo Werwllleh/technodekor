7675f92eadedfbf974e5cafad2e8783d75f33498
Markoni <mi@mworx.ru>
2023-10-30 17:50:28 +0300

    Меню

--- src/js/components/catalog-button.js
+++ src/js/components/catalog-button.js
@@ -1,8 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const button = document.querySelector('.catalog-button');
 
-	button.addEventListener('click', (event) => {
-		event.preventDefault();
-		button.classList.toggle('active');
-	})
 })
---
+++ src/js/components/header.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+	const menuButton = document.querySelector('.catalog-button');
+	const menuBlock = document.querySelector('.header__menu');
+	const menuTopMoreButton = document.querySelector('.header__menu-top-more');
+	const menuTopMore = document.querySelector('.menu__more');
+
+	menuButton.addEventListener('click', (event) => {
+		event.preventDefault();
+		menuButton.classList.toggle('active');
+
+		if (menuButton.classList.contains('active')) {
+			menuBlock.classList.add('active')
+		} else {
+			menuBlock.classList.remove('active')
+			menuTopMoreButton.classList.remove('active');
+			menuTopMore.classList.remove('active')
+
+		}
+	})
+
+	menuTopMoreButton.addEventListener('click', (event) => {
+		event.preventDefault();
+		menuTopMoreButton.classList.toggle('active');
+
+		if (menuTopMoreButton.classList.contains('active')) {
+			menuTopMore.classList.add('active')
+		} else {
+			menuTopMore.classList.remove('active')
+		}
+	})
+
+
+
+});
